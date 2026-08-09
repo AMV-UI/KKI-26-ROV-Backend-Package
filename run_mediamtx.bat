7855ea31b1ba6a46c6e9dff6d4d30f4e8e9ad1ba
vane <88143635+evan052006@users.noreply.github.com>
2026-08-10 02:38:33 +0700

    fix: expose required ports for mediamtx to function

--- run_mediamtx.bat
+++ run_mediamtx.bat
@@ -1,2 +1,7 @@
 @echo off
-docker run --rm -it --network=host -v "%cd%\mediamtx.yml:/mediamtx.yml:ro" bluenviron/mediamtx:1
+docker run --rm -it ^
+  -p 8554:8554 -p 1935:1935 -p 8888:8888 -p 8889:8889 ^
+  -p 8000:8000/udp -p 8001:8001/udp -p 8189:8189/udp ^
+  -p 9997:9997 -p 9996:9996 ^
+  -v "%cd%\mediamtx.yml:/mediamtx.yml:ro" ^
+  bluenviron/mediamtx:1
